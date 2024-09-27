c684c01ac8fc89ace18ce0e33cc5e57b051a6fb7
ozziest <ozziest@users.noreply.github.com>
2024-09-27 21:30:06 +0000

    deploy: axe-api/validator@be3de0a7c48e1804b43b6b00270fff962915223d

---
+++ assets/options.md.DCzrT-MG.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,R as n}from"./chunks/framework.CAothSqJ.js";const g=JSON.parse('{"title":"Options","description":"","frontmatter":{},"headers":[],"relativePath":"options.md","filePath":"options.md"}'),t={name:"options.md"},l=n("",7),p=[l];function e(h,k,E,o,d,r){return a(),i("div",null,p)}const y=s(t,[["render",e]]);export{g as __pageData,y as default};
