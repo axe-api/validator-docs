aa0a25c5b7894e2ac8ac8b57a66bba31c73a6e4f
ozziest <ozziest@users.noreply.github.com>
2024-09-28 11:18:44 +0000

    deploy: axe-api/validator@64111df05c33d275ac5e39cc6197df4532681a37

---
+++ assets/rules.md.B03iNmVk.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,R as t}from"./chunks/framework.CAothSqJ.js";const E=JSON.parse('{"title":"Rules","description":"","frontmatter":{},"headers":[],"relativePath":"rules.md","filePath":"rules.md"}'),e={name:"rules.md"},d=t("",118),l=[d];function h(n,p,k,o,r,c){return a(),i("div",null,l)}const u=s(e,[["render",h]]);export{E as __pageData,u as default};
