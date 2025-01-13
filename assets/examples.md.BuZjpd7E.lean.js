5649df751fb38b0a44c4d926da79e08d77e12c0b
ozziest <ozziest@users.noreply.github.com>
2025-01-13 18:36:22 +0000

    deploy: axe-api/validator@1716993abdf8ce35af754aae3f8202452a6d949c

--- assets/examples.md.BuZjpd7E.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as i,o as a,R as n}from"./chunks/framework.CAothSqJ.js";const c=JSON.parse('{"title":"Demo","description":"","frontmatter":{},"headers":[],"relativePath":"examples.md","filePath":"examples.md"}'),l={name:"examples.md"},h=n("",14),p=[h];function t(k,e,E,d,r,g){return a(),i("div",null,p)}const o=s(l,[["render",t]]);export{c as __pageData,o as default};
